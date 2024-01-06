70284462b26b0051ed4de0f57de92ec68c542e73
seiji@tet.jp <seiji@ttt.jp>
2024-01-06 12:33:32 +0900

    update

---
+++ Sample/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Data.SqlClient;
+namespace Sample;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int timeouts=10*1000;
+        using var con = new SqlConnection("");
+        con.Open();
+        var sqlTask = Task.Run(() => {
+
+            try
+            {
+                using var transaction = new SqlCommand("BEGIN TRAN", con);
+                transaction.ExecuteNonQuery();
+
+
+                using var sqlcommand = new SqlCommand("any query", con);
+                sqlcommand.ExecuteNonQuery();
+
+
+                using var commit = new SqlCommand("COMMIT TRAN", con);
+                commit.ExecuteNonQuery();
+
+            }
+            catch (InvalidOperationException)
+            {
+
+            }
+
+
+        });
+        var tasks = new List<Task>();
+        tasks.Add(sqlTask);
+        tasks.Add(Task.Delay(timeouts) );
+
+        var result=Task.WaitAny(sqlTask);
+        if (result == 1)
+        {
+            Console.WriteLine("timeout");
+        }
+    }
+}
